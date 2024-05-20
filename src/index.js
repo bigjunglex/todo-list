92fdd67fe096bc22a606605d10bd5b40d028cf3a
bigjunglex <gearl00taz@gmail.com>
2024-05-20 20:28:10 +0300

    html skeleton + modal func done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import "./style.css";
 
+const addBtn = document.querySelector('.add_project');
+const projectModal = document.querySelector('#add_project_modal');
+const closeModal = document.querySelector('.closeBtn');
+const projectForm  = document.getElementById('new_project');
+
+addBtn.addEventListener('click', () => {
+    projectModal.show();
+
+})
+
+closeModal.addEventListener('click', () => {
+    projectModal.close();
+})
+
+projectForm.addEventListener('submit', (x) => {
+    x.preventDefault();
+    addProject()
+})
+
+
+function addProject(todo) {
+
+    
+}
