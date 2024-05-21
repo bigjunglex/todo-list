d3893d66e82b12e6f9578b7dec5e7a55c634b708
bigjunglex <gearl00taz@gmail.com>
2024-05-21 17:30:13 +0300

    lists added, todo: add removal of projects, add localstorage, add styling

--- src/index.js
+++ src/index.js
@@ -5,9 +5,19 @@ const projectModal = document.querySelector('#add_project_modal');
 const closeModal = document.querySelector('.closeBtn');
 const projectForm  = document.getElementById('new_project');
 const projectBase = document.querySelector('.projects')
+const todoBase = document.querySelector('.todos')
+
+const projects = [{name: 'defaykt', todos: [
+    {
+        task: 'cast fireball ☄️☄️☄️',
+        completed: true
+    },
+    
+]}];
 
 
-const projects = [];
+// const projects = JSON.parse(localStorage.getItem('projects'))
+
 
 addBtn.addEventListener('click', () => {
     projectModal.show();
@@ -27,9 +37,8 @@ projectForm.addEventListener('submit', (x) => {
 
 function addProject() {
     const projectName = document.getElementById('project_name').value
-    projects.push({name: projectName})
+    projects.push({name: projectName, todos: []})
     document.getElementById('project_name').value = '';
-    
 }
 
 
@@ -41,6 +50,89 @@ function updateProjects() {
         projectItem.classList.add("project");
         projectItem.textContent = project.name;
         projectBase.appendChild(projectItem);
+
+        projectItem.addEventListener('click', () => {
+            viewTodos(project)
+        })
     });
 }
+
+function viewTodos(project) {
+    todoBase.innerHTML = '';
+
+    const todoForm = document.createElement('form');
+    const todoInput = document.createElement('input');
+    const todoButton = document.createElement('button');
+    const todoList = document.createElement('ul');
+
+    todoForm.setAttribute('id', 'todo_form');
+    todoBase.appendChild(todoForm)
+
+    todoInput.setAttribute('id', 'todo_input');
+    todoInput.setAttribute('type', 'text');
+    todoInput.setAttribute('autocomplete', 'off');
+    todoInput.setAttribute('placeholder', 'Plan your moves...');
+    todoInput.classList.add('input');
+    
+    todoButton.setAttribute('type', 'sumbit');
+    todoButton.classList.add('add_todo');
+    todoButton.innerHTML = '☄️';
+    
+    todoList.classList.add('todo_list');
+    
+    todoForm.appendChild(todoInput);
+    todoForm.appendChild(todoButton);
+
+    todoBase.appendChild(todoList)
+
+    if(project.todos) {
+        project.todos.forEach(todo => {
+            const todoEl = document.createElement('li');
+            const removeTodo = document.createElement('button');
+            const todoIndex = project.todos.findIndex(i => i == todo);
+            
+            removeTodo.textContent = '🔥';
+            todoEl.textContent = todo.task;
+            if (todo.completed) todoEl.classList.add('completed');
+
+            
+            
+            todoEl.appendChild(removeTodo)
+            todoList.appendChild(todoEl)
+            
+            removeTodo.addEventListener('click', () => {
+                todoEl.remove()
+                project.todos.splice(todoIndex, 1);
+
+
+            })
+            
+            todoEl.addEventListener('click', () => {
+                todoEl.classList.toggle('completed')
+                if (project.todos[todoIndex]){
+                    project.todos[todoIndex].completed = todoEl.classList.contains('completed')
+                }
+            })
+            
+        })
+    }
+    
+
+    todoForm.addEventListener('submit', (x) => {
+        x.preventDefault();
+        addTodo(project);
+        viewTodos(project);
+    })
+
+}
+
+
+function addTodo(project) {
+    const todoTask = todo_input.value;
+    project.todos.push({task: todoTask, completed: false})
+}
+
+
+
+updateProjects(projects);
 
