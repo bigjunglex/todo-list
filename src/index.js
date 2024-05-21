c617a3275104a30b2e34e41cf1d6aeed59fc138d
bigjunglex <gearl00taz@gmail.com>
2024-05-21 14:38:12 +0300

    projects func added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const addBtn = document.querySelector('.add_project');
 const projectModal = document.querySelector('#add_project_modal');
 const closeModal = document.querySelector('.closeBtn');
 const projectForm  = document.getElementById('new_project');
+const projectBase = document.querySelector('.projects')
+
+
+const projects = [];
 
 addBtn.addEventListener('click', () => {
     projectModal.show();
@@ -16,11 +20,27 @@ closeModal.addEventListener('click', () => {
 
 projectForm.addEventListener('submit', (x) => {
     x.preventDefault();
-    addProject()
+    addProject();
+    updateProjects(projects);
+    projectModal.close();
 })
 
-
-function addProject(todo) {
-
+function addProject() {
+    const projectName = document.getElementById('project_name').value
+    projects.push({name: projectName})
+    document.getElementById('project_name').value = '';
     
 }
+
+
+function updateProjects() {
+    projectBase.innerHTML = '';
+
+    projects.forEach(project => {
+        const projectItem = document.createElement('div');
+        projectItem.classList.add("project");
+        projectItem.textContent = project.name;
+        projectBase.appendChild(projectItem);
+    });
+}
+
